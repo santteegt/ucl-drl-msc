f70e525ebad023dc5a6f3610e4fa41f5190fad93
Santiago Gonzalez <hernangt12re3@gmail.com>
2016-09-05 16:17:28 +0100

    bash files setup to run defined models

---
+++ run-v1-random.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -Rf ddpg-results/experiment1/
+nohup python src/run.py --outdir ddpg-results/experiement1 --env CollaborativeFiltering-v1 --train 100 --test 100 --tmax 2314 --total 2163000 --random --create_index --wp_total_actions 1682 --wp_action_set_file "data/ml-1m/items_collection.npy" --knn_backend sklearn --nn_index_file indexStorageTest --knn 84 --skip_space_norm >> run.log &
+echo $! > pid
+
+sleep 1
+
+echo Process started. Logs are written in run.log
---
+++ run-v3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -Rf ddpg-results/experiment1/
+nohup python src/run.py --outdir ddpg-results/experiement1 --env CollaborativeFiltering-v3 --train 100 --test 100 --tmax 2314 --total 2163000 --fmpolicy --create_index --wp_total_actions 1682 --wp_action_set_file "data/ml-100k/embeddings-movielens100k.csv" --knn 84 --skip_space_norm >> run.log &
+echo $! > pid
+
+sleep 1
+
+echo Process started. Logs are written in run.log

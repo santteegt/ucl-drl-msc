645c47420c1f972b56c6685eadd68bd5eb96613d
Santiago Gonzalez <hernangt12re3@gmail.com>
2016-09-07 04:43:16 +0100

    Updating run.sh for experiment

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -Rf ddpg-results/experiment1/
-nohup python src/run.py --outdir ddpg-results/experiement1 --env CollaborativeFiltering-v0 --train 20 --test 1 --tmax 2314 --total 2163000 --wolpertinger --create_index --wp_total_actions 3883 --wp_action_set_file "data/embeddings-movielens1m.csv" --knn_backend sklearn --nn_index_file indexStorageTest --knn 194 >> run.log &
+nohup python src/run.py --outdir ddpg-results/experiement1 --env CollaborativeFiltering-v0 --train 100 --test 100 --tmax 2314 --total 2163000 --wolpertinger --create_index --wp_total_actions 3883 --wp_action_set_file "data/embeddings-movielens1m.csv" --knn_backend sklearn --nn_index_file indexStorageTest --knn 194  --skip_space_norm >> run.log &
 echo $! > pid
 
 sleep 1

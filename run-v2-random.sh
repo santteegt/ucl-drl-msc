1f1be3106b99336da9136795f65009935d798382
Santiago Gonzalez <hernangt12re3@gmail.com>
2016-09-09 05:15:33 +0100

    Files already moved to bin folder

--- run-v2-random.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -Rf ddpg-results/experiment1/
-nohup python src/run.py --outdir ddpg-results/experiement1 --env CollaborativeFiltering-v2 --train 100 --test 100 --tmax 2314 --total 2163000 --random --create_index --wp_total_actions 1682 --wp_action_set_file "data/ml-100k/embeddings-movielens100k.csv" --knn_backend sklearn --nn_index_file indexStorageTest --knn 84 --skip_space_norm >> run.log &
-echo $! > pid
-
-sleep 1
-
-echo Process started. Logs are written in run.log
